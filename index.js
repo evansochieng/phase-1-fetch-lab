1fd3a88753df019b1ea51142b9d04148a11ff3fb
Evans Ochieng <evansochieng121@gmail.com>
2022-08-22 19:04:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return (
+    fetch('https://anapioficeandfire.com/api/books') // make a fetch request
+    .then( (response) => response.json() ) //convert to JSON
+    .then( (result) => renderBooks(result) ) //call renderBooks function
+  )
 }
 
 function renderBooks(books) {
@@ -15,3 +19,20 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+//Function to add the number of pages
+
+// Create a function that adds pages
+function sumPages(pageZero, book){
+  return pageZero + book.numberOfPages;
+}
+
+/* Make a fetch request, call the function to sum the pages,
+save the total to a variable */
+fetch('https://anapioficeandfire.com/api/books') // make a fetch request
+    .then( (response) => response.json() ) //convert to JSON
+    .then( (result) => {
+      const totalPages = result.reduce(sumPages, 0);//sum pages
+      console.log(totalPages);  // log the total pages
+      return totalPages;
+    } )
